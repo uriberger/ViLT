82e12cf4e39cd23f9e674a68e1de1997d37bc849
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-04-03 17:13:20 +0300

    Enabled pytorch lightining back on

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import os
 import copy
-#import pytorch_lightning as pl
-from transformers import Trainer
+import pytorch_lightning as pl
 
 from vilt.config import ex
 from vilt.modules import ViLTransformerSS
@@ -11,7 +10,7 @@ from vilt.datamodules.multitask_datamodule import MTDataModule
 @ex.automain
 def main(_config):
     _config = copy.deepcopy(_config)
-    #pl.seed_everything(_config["seed"])
+    pl.seed_everything(_config["seed"])
 
     dm = MTDataModule(_config, dist=True)
 
@@ -19,20 +18,20 @@ def main(_config):
     exp_name = f'{_config["exp_name"]}'
 
     os.makedirs(_config["log_dir"], exist_ok=True)
-    # checkpoint_callback = pl.callbacks.ModelCheckpoint(
-    #     save_top_k=1,
-    #     verbose=True,
-    #     monitor="val/the_metric",
-    #     mode="max",
-    #     save_last=True,
-    # )
-    # logger = pl.loggers.TensorBoardLogger(
-    #     _config["log_dir"],
-    #     name=f'{exp_name}_seed{_config["seed"]}_from_{_config["load_path"].split("/")[-1][:-5]}',
-    # )
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        save_top_k=1,
+        verbose=True,
+        monitor="val/the_metric",
+        mode="max",
+        save_last=True,
+    )
+    logger = pl.loggers.TensorBoardLogger(
+        _config["log_dir"],
+        name=f'{exp_name}_seed{_config["seed"]}_from_{_config["load_path"].split("/")[-1][:-5]}',
+    )
 
-    # lr_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
-    # callbacks = [checkpoint_callback, lr_callback]
+    lr_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
+    callbacks = [checkpoint_callback, lr_callback]
 
     num_gpus = (
         _config["num_gpus"]
@@ -46,8 +45,7 @@ def main(_config):
 
     max_steps = _config["max_steps"] if _config["max_steps"] is not None else None
 
-    # trainer = pl.Trainer(
-    trainer = Trainer(
+    trainer = pl.Trainer(
         gpus=_config["num_gpus"],
         num_nodes=_config["num_nodes"],
         precision=_config["precision"],
@@ -56,8 +54,8 @@ def main(_config):
         deterministic=True,
         max_epochs=_config["max_epoch"] if max_steps is None else 1000,
         max_steps=max_steps,
-        # callbacks=callbacks,
-        # logger=logger,
+        callbacks=callbacks,
+        logger=logger,
         prepare_data_per_node=False,
         replace_sampler_ddp=False,
         accumulate_grad_batches=grad_steps,
