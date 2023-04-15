7047066a52ee034a3c51aa8f1ce3e27e7aba3454
Uri Berger <uriber@cs.huji.ac.il>
2023-04-15 15:10:12 +0300

    Now all checkpoints will be saved

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def main(_config):
 
     os.makedirs(_config["log_dir"], exist_ok=True)
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        save_top_k=1,
+        save_top_k=-1,
         verbose=True,
         monitor="val/the_metric",
         mode="max",
