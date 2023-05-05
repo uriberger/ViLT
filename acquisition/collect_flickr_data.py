562e8d1d0a1c87dbd36a64a86b3185cdba1af4d0
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-05-05 22:08:56 +0300

    Added acquisition related code

---
+++ acquisition/collect_flickr_data.py
@@ -0,0 +1,22 @@
+import os
+import json
+from acquisition.config import cache_dir
+
+def collect_flickr_data(root_path):
+    file_name = 'flickr_sentences.json'
+    file_path = os.path.join(cache_dir, file_name)
+    if os.path.isfile(file_path):
+        with open(file_path, 'r'):
+            sentences = json.load(fp)
+    else:
+        sentences = []
+        tokens_file_path = os.path.join(root_path, 'tokens', 'results_20130124.token')
+        with open(tokens_file_path, encoding='utf-8') as fp:
+            for line in fp:
+                split_line = line.strip().split('g#')
+                caption = split_line[1].split('\t')[1]  # The first token is caption number
+                sentences.append(caption)
+        with open(file_path, 'w') as fp:
+            fp.write(json.dumps(sentences))
+
+    return sentences
---
+++ acquisition/config.py
@@ -0,0 +1,2 @@
+cache_dir = 'cache_dir'
+flickr_root_path = '/cs/labs/oabend/uriber/datasets/flickr30'
