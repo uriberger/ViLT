ce02c0a136beca796999fc916f6d5806e214ed70
Uri Berger <uri.berger2@mail.huji.ac.il>
2024-03-24 00:41:21 +1100

    Fixed a small bug

--- acquisition/embed.py
+++ acquisition/embed.py
@@ -37,7 +37,7 @@ def extract_embeddings(sentences, model, tokenizer):
 
 def embed_word(word, model, tokenizer, agg_method):
     with torch.no_grad():
-        input_ids = tokenizer(word, padding=True, return_tensors='pt').input_ids
+        input_ids = tokenizer(word, padding=True, return_tensors='pt').input_ids.to(model.device)
     embeddings = model.text_embeddings(input_ids[:, 1:-1])[0, :, :]
     return agg_vectors(embeddings, agg_method)
 
